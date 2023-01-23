0456d835672aaf14fb5dc00b20867ecfbe899eec
Andrew Morris <andrew.morris.crypto@gmail.com>
2023-01-23 10:42:03 +0000

    Add webpage

---
+++ app.js
@@ -0,0 +1,9 @@
+function getSpecies() {
+  let species = prompt("What is your species?");
+  console.log("The species is: " + species);
+};
+
+function getNutriment() {
+  let nutriment = prompt("What is your nutriment?");
+  console.log("Your nutriment is " + nutriment);
+}
