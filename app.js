be456406a6b1704c72ba2ea3c4d5942e69d9e419
Andrew Morris <andrew.morris.crypto@gmail.com>
2023-01-23 11:12:06 +0000

    Add colours and summary of questions

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 function getSpecies() {
   let species = prompt("What is your species?");
-  console.log("The species is: " + species);
+  //console.log("The species is: " + species);
+  document.write("The species is: " + species);
 };
 
 function getNutriment() {
   let nutriment = prompt("What is your nutriment?");
   console.log("Your nutriment is " + nutriment);
+  document.write("Your nutriment is " + nutriment);
 }
+
+let ans1, ans2, ans3, ans4;
+
+function askQuestion(question) {
+  let answer = prompt(question + "?");
+  //console.log(question);
+  console.log(question + ": " + answer);
+  document.write(question + ": " + answer);
+  return answer;
+};
